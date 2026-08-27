b3a1ce3a966df0ac72b2e05640cdfb26f1f8492b
jhordan <jhordangabrielanjos@gmail.com>
2026-08-27 11:45:58 -0300

    Update cliente.js

--- cliente.js
+++ cliente.js
@@ -388,6 +388,21 @@ async function carregarConfiguracaoEstabelecimento() {
 
         }
 
+        // ======================================
+// IMAGEM DE FUNDO
+// ======================================
+
+if (estabelecimento.imagem_fundo) {
+
+    document.body.style.backgroundImage =
+        `url("${estabelecimento.imagem_fundo}")`;
+
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundRepeat = "no-repeat";
+
+}
 
         // ======================================
         // INFORMAÇÕES
